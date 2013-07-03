d3f501b9a434bb813999d41f8a11a2db096c3791
pgonzalezAlivebox <pgonzalez@alivebox.com>
2013-07-02 20:57:10 -0600

    REST integrate IMCOMPLETE commit

---
+++ compile-templates/home.js
@@ -0,0 +1,69 @@
+Ember.TEMPLATES["home"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [3,'>= 1.0.0-rc.4'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  var buffer = '', stack1, hashTypes, hashContexts, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  var buffer = '', hashTypes, hashContexts;
+  data.buffer.push("\n<li>");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "user", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</li>\n");
+  return buffer;
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes, hashContexts;
+  data.buffer.push("\n    <ul>\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers.each.call(depth0, "user", "in", "users", {hash:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    </ul>\n");
+  return buffer;
+  }
+function program4(depth0,data) {
+  
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
+  data.buffer.push("\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0,depth0],types:["STRING","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "user", "team", options) : helperMissing.call(depth0, "linkTo", "user", "team", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n        ");
+  return buffer;
+  }
+function program5(depth0,data) {
+  
+  var buffer = '', hashTypes, hashContexts;
+  data.buffer.push("\n    <p> ");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "user.email", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push(" </p>\n        ");
+  return buffer;
+  }
+
+function program7(depth0,data) {
+  
+  
+  data.buffer.push("\n    <p class=\"loading\">...Loading teams</p>\n");
+  }
+
+  data.buffer.push("<h3>Basic List</h3>\n\n<ul>\n");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers.each.call(depth0, "user", "in", "info", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n</ul>\n\n<h3>Teams</h3>\n");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "users.isLoaded", {hash:{},inverse:self.program(7, program7, data),fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  return buffer;
+  
+});
--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
     this.route("home", { path: "/" });
+    this.resource('test', function(){
+        this.route('summary')
+    });
     this.resource('team', { path: '/team/:team_id' });
 });
 
@@ -11,44 +14,56 @@ App.HomeRoute = Ember.Route.extend({
     },
     setupController: function(controller, model){
         controller.set('info', model)
-        controller.set('teams', App.Team.find());
+        controller.set('teams', App.User.find());
     }
 });
 
 /** STORES */
 
+DS.RESTAdapter.reopen({
+    url: 'http://localhost:8000/main'
+//    url:'/ws/ember'
+});
+
 App.Store = DS.Store.extend({
-    revision: 12,
-    adapter: 'DS.FixtureAdapter'
+    revision: 13
+//    url:"http://localhost:8000/main"
+//    adapter: 'DS.FixtureAdapter'
 });
 
 /** MODELS */
 
-App.Team = DS.Model.extend({
+App.User = DS.Model.extend({
     name: DS.attr('string'),
-    colors: DS.attr('string')
+    email: DS.attr('string')
 })
 
 /**FIXTURES*/
 
-App.Team.FIXTURES = [{
-    id: 1,
-    name: 'Celtics',
-    colors: 'Green, White'
-}, {
-    id: 2,
-    name: 'Lakers',
-    colors: 'Yellow, Black'
-}, {
-    id: 3,
-    name: 'Bulls',
-    colors: 'Red, Black'
-}, {
+App.User.FIXTURES = [{
+    pk: 4,
+    name: "a",
+    email: "jclare@alivebox.com",
+    entity_status: 0,
     id: 4,
-    name: 'Mavericks',
-    colors: 'Blue, White'
-}, {
-    id: 5,
-    name: 'Spurs',
-    colors: 'Black, Grey, White'
-}];
+    password: "ffd534f67f52647378dc626d28c25746",
+    default_group: 32767,
+    session_key: "1"
+//}, {
+//    id: 2,
+//    name: 'Lakers',
+//    email: 'Yellow, Black'
+//}, {
+//    id: 3,
+//    name: 'Bulls',
+//    email: 'Red, Black'
+//}, {
+//    id: 4,
+//    name: 'Mavericks',
+//    email: 'Blue, White'
+//}, {
+//    id: 5,
+//    name: 'Spurs',
+//    email: 'Black, Grey, White'
+//
+ }];
